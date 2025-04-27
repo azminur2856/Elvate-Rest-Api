168a328bc94a928d530eb9adcdd277ba965deb3d
Towsif <towsif1528@gmail.com>
2025-04-27 21:50:14 +0600

    create product/Towsif

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create_product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private productService: ProductService) {}
+
+  @Post()
+  async createProduct(@Body() body: CreateProductDto) {
+    return this.productService.create(body);
+    // return 'hello';
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './product.entity';
+import { Repository } from 'typeorm';
+import { CreateProductDto } from './dto/create_product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product) private productRepository: Repository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    const product = this.productRepository.create(createProductDto);
+    return this.productRepository.save(product);
+  }
+}
