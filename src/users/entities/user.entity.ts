e3bbf2913d690dce78f671f922ed8a18ba737a22
AbirBokhtiar <abirbokhtiar107#gmail.com>
2025-04-28 04:51:12 +0600

    order-shipment

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,3 +1,11 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, Timestamp } from "typeorm";
+import { OrderEntity } from "src/orders/entities/order.entity";
+import { Column, CreateDateColumn, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn, Timestamp } from "typeorm";
 
-export class UserEntity {}
+@Entity({ name:'users'})
+export class UserEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @OneToMany(() => OrderEntity, (order) => order.updatedBy)
+    ordersUpdateBy: OrderEntity[];
+}
