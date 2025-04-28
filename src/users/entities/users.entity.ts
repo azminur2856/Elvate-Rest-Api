6a5c18c0ddeb4ca3b9ff214ae50ac25d66e63ced
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-04-29 04:24:20 +0600

    google OAuth Add

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -24,7 +24,7 @@ export class Users {
   @Column({ type: 'varchar', length: 255, nullable: true })
   lastName?: string;
 
-  @Column()
+  @Column({ type: 'date', nullable: true })
   dob: Date;
 
   @Column({ type: 'varchar', length: 255, unique: true })
