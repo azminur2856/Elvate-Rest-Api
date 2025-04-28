00623cd5db8c0bb63954e5d6b516900aaa8c48f1
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-04-29 04:39:39 +0600

    update

--- src/activity-logs/activity-logs.controller.ts
+++ src/activity-logs/activity-logs.controller.ts
@@ -9,16 +9,21 @@ import {
 } from '@nestjs/common';
 import { ActivityLogsService } from './activity-logs.service';
 import { CreateActivityLogDto } from './dto/create-activity-log.dto';
+import { Public } from 'src/auth/decorators/public.decorator';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/users/enums/role.enum';
 
 @Controller('activity-logs')
 export class ActivityLogsController {
   constructor(private activityLogsService: ActivityLogsService) {}
 
+  @Roles(Role.ADMIN)
   @Get('getAllActivityLogs')
   getAllActivityLogs() {
     return this.activityLogsService.getAllActivityLogs();
   }
 
+  @Roles(Role.ADMIN)
   @Get('getUserActivityLogs/:userId')
   getUserActivityLogs(@Param('userId') userId: string) {
     return this.activityLogsService.getUserActivityLog(userId);
@@ -33,7 +38,7 @@ export class ActivityLogsController {
     return this.activityLogsService.createActivityLog(createActivityLogDto);
   }
 
-  //Public route for creating unauthorized action logs
+  @Public()
   @Post('createUnauthorizedActivityLog')
   async createUnauthorizedActionLog(
     @Body(ValidationPipe) createActivityLogDto: CreateActivityLogDto,
