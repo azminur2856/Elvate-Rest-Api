996758c813260fb5f38515de63920ab5dd82aca7
khalidmahamud <22-46601-1@student.aiub.edu>
2025-04-28 19:43:16 +0600

    feat: refactor admin module by adding AdminActivity entity and AdminRole guard, and removing unused interfaces

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,17 +1,46 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Import controllers
 import { AdminUsersController } from './controllers/admin-users.controller';
 import { AdminProductsController } from './controllers/admin-products.controller';
 import { AdminOrdersController } from './controllers/admin-orders.controller';
 import { AdminDashboardController } from './controllers/admin-dashboard.controller';
 import { AdminReportsController } from './controllers/admin-reports.controller';
+
+// Import services
 import { AdminUsersService } from './services/admin-users.service';
 import { AdminProductsService } from './services/admin-products.service';
 import { AdminOrdersService } from './services/admin-orders.service';
 import { AdminDashboardService } from './services/admin-dashboard.service';
 import { AdminReportsService } from './services/admin-reports.service';
 
+// Import entities
+import { AdminActivity } from './entities/admin-activity.entity';
+import { Users } from 'src/users/entities/users.entity';
+
 @Module({
-  controllers: [AdminUsersController, AdminProductsController, AdminOrdersController, AdminDashboardController, AdminReportsController],
-  providers: [AdminUsersService, AdminProductsService, AdminOrdersService, AdminDashboardService, AdminReportsService]
+  imports: [TypeOrmModule.forFeature([AdminActivity, Users]), Users],
+  controllers: [
+    AdminUsersController,
+    AdminProductsController,
+    AdminOrdersController,
+    AdminDashboardController,
+    AdminReportsController,
+  ],
+  providers: [
+    AdminUsersService,
+    AdminProductsService,
+    AdminOrdersService,
+    AdminDashboardService,
+    AdminReportsService,
+  ],
+  exports: [
+    AdminUsersService,
+    AdminProductsService,
+    AdminOrdersService,
+    AdminDashboardService,
+    AdminReportsService,
+  ],
 })
 export class AdminModule {}
