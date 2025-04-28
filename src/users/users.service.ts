8f5df1eed82a55b70fb5cd74f09c29c77c1797a0
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-04-28 21:51:32 +0600

    update users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -121,14 +121,18 @@ export class UsersService {
     if (!user) {
       throw new NotFoundException(`User with ID ${id} not found`);
     }
-    if (
-      await this.userRepository.findOne({
+
+    if (updateUserDto.phone) {
+      const checkPhone = await this.userRepository.findOne({
         where: { phone: updateUserDto.phone },
-      })
-    ) {
-      throw new BadRequestException(
-        `User with this phone ${updateUserDto.phone} already exists`,
-      );
+      });
+
+      if (checkPhone && checkPhone.id !== id) {
+        throw new BadRequestException(
+          `User with this phone ${updateUserDto.phone} already exists`,
+        );
+      }
+      updateUserDto.isPhoneVerified = false; // Set isPhoneVerified to false if phone is updated
     }
 
     const result = await this.userRepository.update(id, updateUserDto);
@@ -169,6 +173,11 @@ export class UsersService {
       user: await this.userRepository.findOne({ where: { id: adminId } }),
     };
     await this.activityLogsService.createActivityLog(activityLog);
+
+    return {
+      message: 'User role updated successfully',
+      userAffected: result.affected,
+    };
   }
 
   async deteteUser(id: string, adminId: string) {
