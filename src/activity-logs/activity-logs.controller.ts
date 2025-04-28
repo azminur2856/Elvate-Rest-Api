ff3fa42335419494bae88feef0f93aa35c1b677b
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-04-28 15:39:27 +0600

    update activity-logs module

--- src/activity-logs/activity-logs.controller.ts
+++ src/activity-logs/activity-logs.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ActivityLogsService } from './activity-logs.service';
 
 @Controller('activity-logs')
-export class ActivityLogsController {}
+export class ActivityLogsController {
+  constructor(private activityLogsService: ActivityLogsService) {}
+
+  @Get('getAllActivityLogs')
+  getAllActivityLogs() {
+    return this.activityLogsService.getAllActivityLogs();
+  }
+
+  @Get('getUserActivityLogs/:userId')
+  getUserActivityLogs(@Param('userId') userId: string) {
+    return this.activityLogsService.getUserActivityLog(userId);
+  }
+}
--- src/activity-logs/activity-logs.module.ts
+++ src/activity-logs/activity-logs.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ActivityLogsController } from './activity-logs.controller';
 import { ActivityLogsService } from './activity-logs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ActivityLog } from './entities/activity-logs.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ActivityLog])],
   controllers: [ActivityLogsController],
-  providers: [ActivityLogsService]
+  providers: [ActivityLogsService],
 })
 export class ActivityLogsModule {}
