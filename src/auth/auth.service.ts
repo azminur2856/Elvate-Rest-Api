301a3495ede1bf4192ea2562086c7f9069f17bda
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-04-28 22:54:06 +0600

    local auth done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { compare } from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    const isPasswordMatch = await compare(password, user.password);
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException('Invalid password');
+    }
+    return { id: user.id };
+  }
+}
