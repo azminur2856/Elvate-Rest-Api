16b6d4f68a0518253be7c871837537557b931f31
AZMINUR RAHMAN <97751681+azminur2856@users.noreply.github.com>
2025-04-28 22:54:06 +0600

    local auth done

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UsersService } from 'src/users/users.service';
+import { LocalStrategy } from './strategies/local.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from 'src/users/entities/users.entity';
+import { ActivityLogsService } from 'src/activity-logs/activity-logs.service';
+import { ActivityLog } from 'src/activity-logs/entities/activity-logs.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Users, ActivityLog])],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, UsersService, ActivityLogsService, LocalStrategy],
 })
 export class AuthModule {}
